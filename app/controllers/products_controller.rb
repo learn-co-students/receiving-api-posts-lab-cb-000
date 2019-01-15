0afdf65190ff9e415ea8e400e068a32a43cc8d1f
Chris Cardle <cdcardle@gmail.com>
2019-01-15 16:12:04 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,8 +18,8 @@ class ProductsController < ApplicationController
   end
 
   def create
-    Product.create(product_params)
-    redirect_to products_path
+    @products = Product.create(product_params)
+    render json: @products, status: 201
   end
 
   def show
